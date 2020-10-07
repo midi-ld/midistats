1ae6feb4f345e9ce1e2cf6d517df9dbeb39b291a
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2020-10-07 11:16:34 +0100

    aggregated per dataset

--- stats.py
+++ stats.py
@@ -1,29 +1,63 @@
 # /usr/bin/env python
 from mido import MidiFile
 import sys
+import os.path
 
-in_file = sys.argv[1]
-
-file_stats = {'filename' : in_file}
-with MidiFile(in_file) as mid:
-    print("Tracks: {}".format(len(mid.tracks)))
-    for i, track in enumerate(mid.tracks):
-        track_stats = {'track_nr' : i, 'track_label' : track.name}
-        # track_stats = {}
-        for msg in track:
-            if msg.type not in track_stats:
-                track_stats[msg.type] = 1
-            else:
-                track_stats[msg.type] += 1
-            if msg.type not in file_stats:
-                file_stats[msg.type] = 1
-            else:
-                file_stats[msg.type] += 1
-        print(track_stats)
-        print(file_stats)
-
-# print all file stats in one CSV row
-print(','.join(str(v) for v in file_stats.values()))
+dataset_stats = {}
+dataset_stats['files'] = 0
+for i in range(128):
+    dataset_stats['note_' + str(i)] = 0
+for i in range(128):
+    dataset_stats['program_' + str(i)] = 0
 
 
+def process_midi(path, file):
+    # file_stats = {'filename' : os.path.join(path, file)}
+    try:
+        with MidiFile(os.path.join(path, file)) as mid:
+            for i, track in enumerate(mid.tracks):
+                for msg in track:
+                    # General message counts
+                    # if msg.type not in file_stats:
+                    #     file_stats[msg.type] = 1
+                    # else:
+                    #     file_stats[msg.type] += 1
+
+                    # pitches
+                    if msg.type == 'note_on':
+                        dataset_stats['note_' + str(msg.note)] += 1
+                        #print('note_on: {}'.format(msg.note))
+                    # instruments
+                    if msg.type == 'program_change':
+                        dataset_stats['program_' + str(msg.program)] += 1
+                        #print('program_change: {}'.format(msg.program))
+                    # tempo
+                    if msg.type == 'set_tempo':
+                        if str('tempo_' + str(msg.tempo)) not in dataset_stats:
+                                dataset_stats['tempo_' + str(msg.tempo)] = 0
+                        dataset_stats['tempo_' + str(msg.tempo)] += 1
+                        # print('set_tempo: {}'.format(msg.tempo))
+                    # time signature
+                    if msg.type == 'time_signature':
+                        # print('time_signature: {}'.format(str(msg.numerator) + '/' + str(msg.denominator)))
+                        if str('time_signature_' + str(msg.numerator) + '/' + str(msg.denominator)) not in dataset_stats:
+                            dataset_stats['time_signature_' + str(msg.numerator) + '/' + str(msg.denominator)] = 0
+                        dataset_stats['time_signature_' + str(msg.numerator) + '/' + str(msg.denominator)] += 1
+    except (OSError, EOFError, KeyError) as e:
+        print("Error in MIDI file: {}".format(e))
+        pass
+    except:
+        print("Unexpected error: {}".format(sys.exc_info()[0]))
+
+
+    # Output
+    # print(','.join(str(v) for v in file_stats.values()))
+
+for path, directories, files in os.walk(sys.argv[1]):
+    for file in files:
+        if file.endswith('.mid'):
+            process_midi(path, file)
+            dataset_stats['files'] += 1
+
+print(dataset_stats)
 exit(0)
